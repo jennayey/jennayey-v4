aa8f8fbe27803abdfb7f1cf642b68862e5efab17
JENNAPC\jenna <jennayey@gmail.com>
2025-03-18 21:15:32 +0800

    added arrows

--- components/ui/sidequestCard.tsx
+++ components/ui/sidequestCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ArrowUpRight } from "lucide-react";
 
 interface Props {
   title: string;
@@ -9,13 +10,14 @@ export default function Sidequest({ title, description, url }: Props) {
   return (
     <div className="text-sm border-1 border-dashed border-zinc-700">
       {/* Title card */}
-      <div className="bg-zinc-800 text-zinc-50 p-2">
+      <div className="flex justify-between group bg-zinc-800 text-zinc-50 hover:bg-yellow-500 hover:text-zinc-800 hover: p-2">
         <a
           href={url}
-          className="underline decoration-yellow-500 underline-offset-4 hover:text-yellow-500"
+          className="underline group-hover:decoration-zinc-800 decoration-yellow-500 underline-offset-4 "
         >
           {title}
         </a>
+        <ArrowUpRight size={16} className="text-yellow-500 group-hover:text-zinc-800 "/>
       </div>
       <div className="text-zinc-50 p-2">
         <p>{description}</p>
@@ -23,3 +25,4 @@ export default function Sidequest({ title, description, url }: Props) {
     </div>
   );
 }
+
